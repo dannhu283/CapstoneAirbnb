d9d699db69166e296bfdf5271ba9e18b8378a220
dannhu283 <dannhu2803@gmail.com>
2023-10-22 17:44:51 +0700

    add footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,47 @@
 import React from "react";
+import { Container, List, ListItem, Grid } from "@mui/material";
+import { LiFooter, TitleF } from "./index";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+          <TitleF>Giới thiệu</TitleF>
+          <List>
+            <ListItem>
+              <LiFooter href="#">Phương thức hoạt động của Airbnb</LiFooter>
+            </ListItem>
+            <ListItem>
+              <LiFooter href="#">Trang tin tức</LiFooter>
+            </ListItem>
+            <ListItem>
+              <LiFooter href="#">Nhà đầu tư</LiFooter>
+            </ListItem>
+            <ListItem>
+              <LiFooter href="#">Airbnb Plus</LiFooter>
+            </ListItem>
+            <ListItem>
+              <LiFooter href="#">Airbnb Luxe</LiFooter>
+            </ListItem>
+            <ListItem>
+              <LiFooter href="#">HotelTonight</LiFooter>
+            </ListItem>
+            <ListItem>
+              <LiFooter href="#">Airbnb for Work</LiFooter>
+            </ListItem>
+            <ListItem>
+              <LiFooter href="#">Nhờ có Host, mọi điều đều có thể</LiFooter>
+            </ListItem>
+            <ListItem>
+              <LiFooter href="#">Cơ hội nghề nghiệp</LiFooter>
+            </ListItem>
+            <ListItem>
+              <LiFooter href="#">Thư của nhà sáng lập</LiFooter>
+            </ListItem>
+          </List>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,1 +1,17 @@
+import styled from "styled-components";
+
 export { default } from "./Footer";
+
+export const LiFooter = styled.a`
+  color: black;
+  text-decoration: none;
+  margin-bottom: 4px;
+  &:hover {
+    color: #0984e3;
+  }
+`;
+
+export const TitleF = styled.p`
+  font-size: 20px;
+  font-weight: bold;
+`;
