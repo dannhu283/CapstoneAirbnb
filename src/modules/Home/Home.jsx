154b44f7800463a3dece19b58437ce4b066d5cb0
dannhu283 <dannhu2803@gmail.com>
2023-10-25 19:44:45 +0700

    add

--- src/modules/Home/Home.jsx
+++ src/modules/Home/Home.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
 
 export default function Home() {
-  return (
-    <Box height={200}>
-      <Typography sx={{ margin: "1000px" }}>Home</Typography>
-    </Box>
-  );
+  return <div>Home</div>;
 }
