71a85b8f308536b8bce135451c46154db68980c2
dannhu283 <dannhu2803@gmail.com>
2023-11-11 17:50:49 +0700

    fix footer

--- src/Components/Footer/components/HeaderFooter/HeaderFooter.jsx
+++ src/Components/Footer/components/HeaderFooter/HeaderFooter.jsx
@@ -12,7 +12,7 @@ export default function HeaderFooter() {
           Ở bất cứ đâu
         </Typography>
         <Grid container spacing={2}>
-          <Grid item xs={3}>
+          <Grid item md={3} xs={6}>
             <div className={style.headerFooterItem}>
               <img
                 src="https://a0.muscache.com/im/pictures/298b88b8-042b-4fe4-91dc-db8a29840759.jpg?im_w=720"
@@ -22,7 +22,7 @@ export default function HeaderFooter() {
               <p className={style.title}>Toàn bộ nhà</p>
             </div>
           </Grid>
-          <Grid item xs={3}>
+          <Grid item md={3} xs={6}>
             <div className={style.headerFooterItem}>
               <img
                 src="https://a0.muscache.com/im/pictures/miso/Hosting-618869107520371080/original/7fc1a193-f142-4010-a302-d9112344ff04.jpeg?im_w=720"
@@ -32,7 +32,7 @@ export default function HeaderFooter() {
               <p className={style.title}>Chỗ ở độc đáo</p>
             </div>
           </Grid>
-          <Grid item xs={3}>
+          <Grid item md={3} xs={6}>
             <div className={style.headerFooterItem}>
               <img
                 src="https://a0.muscache.com/im/pictures/miso/Hosting-46890950/original/a4d94380-466b-455f-af2c-0e3d816b5bc5.jpeg?im_w=720"
@@ -42,7 +42,7 @@ export default function HeaderFooter() {
               <p className={style.title}>Chỗ ở thiên nhiên</p>
             </div>
           </Grid>
-          <Grid item xs={3}>
+          <Grid item md={3} xs={6}>
             <div className={style.headerFooterItem}>
               <img
                 src="https://a0.muscache.com/im/pictures/df9c73e6-c3e7-48d0-9aa8-1d432bdfff1d.jpg?im_w=960"
