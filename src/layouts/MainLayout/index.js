a2c5f7a570fa0cd81df795645bbab9d0f1488163
dannhu283 <dannhu2803@gmail.com>
2023-11-15 23:16:08 +0700

    fix backtotop and add link rel

--- src/layouts/MainLayout/index.js
+++ src/layouts/MainLayout/index.js
@@ -3,13 +3,13 @@ import styled from "@emotion/styled";
 export { default } from "./MainLayout";
 
 export const BackToTop = styled.div`
-  color: #3ae374;
+  color: #f43f5e;
   position: fixed;
   bottom: 20px;
   right: 20px;
   cursor: pointer;
   transition: opacity 0.3s;
   &:hover {
-    color: #ffb8b8;
+    color: #f48f5e;
   }
 `;
